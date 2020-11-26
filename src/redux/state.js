9bd327733ea9707f7a8851cc238432ad3f6df645
Anton-Klochkov <69051844+Anton-Klochkov@users.noreply.github.com>
2020-11-26 13:20:38 +0300

    restartSystem

---
+++ src/redux/state.js
@@ -0,0 +1,29 @@
+let state = {
+    profilePage: {
+        postData: [
+            { id: '1', message: "My first day in a new city", likesCount: 78 },
+            { id: '2', message: "Friends Please go to the lake", likesCount: 59 },
+            { id: '3', message: "My like movie, Ender's Game", likesCount: 68 },
+            { id: '4', message: "Good day, my friends called me out on the street", likesCount: 36 },
+            { id: '5', message: "Hallo World", likesCount: 24 }
+          ]
+    },
+    messagesPage: {
+        dialogsData: [
+            { id: '1', name: 'Viktor' },
+            { id: '2', name: 'Sveta' },
+            { id: '3', name: 'Sasha' },
+            { id: '4', name: 'Dmitriy' },
+            { id: '5', name: 'Valera' },
+            { id: '6', name: 'Irina' }
+          ],
+        messagesData: [
+            { id: '1', message: "We are you from" },
+            { id: '2', message: "I'm from Russia" },
+            { id: '3', message: "Go to steer" },
+            { id: '4', message: "First react project" },
+          ]
+        }
+}  
+
+export default state;
